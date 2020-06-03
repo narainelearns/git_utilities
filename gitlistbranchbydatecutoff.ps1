246906e15eafa34f5b00a563cc7ae7d61ecd09b0
Shanmuga Narayanan Pitchaipillai <59262720+narainelearns@users.noreply.github.com>
2020-06-02 23:58:16 -0400

    cut off date param updated
    cut off date param updated

--- gitlistbranchbydatecutoff.ps1
+++ gitlistbranchbydatecutoff.ps1
@@ -1,4 +1,5 @@
 
+$cutoffdate = "2020-03-01" $cutoffdate=[datetime]::ParseExact("$cutoffdate", 'yyyy-MM-dd', $null);
 git for-each-ref --merged PRODUCTION --sort='-committerdate' --format='%(committerdate:short), %(refname:short)' refs/remotes/origin/promotion  | foreach-object { $branchdate=[datetime]::ParseExact($_.Split(",")[0], 'yyyy-MM-dd', $null ); $branchname=$_.Split(",")[1].Replace("origin/",""); if ($branchdate -le $cutoffdate) {write-output "Older branch:$branchdate ::$branchname"}}
 
 
